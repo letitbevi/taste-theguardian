ffafcfe6c5ed437cea4f0831b87e02083531e156
Joanne Tsui <letitbevi@gmail.com>
2016-11-08 11:48:22 +0800

    Add two examples

--- theguardian_api.py
+++ theguardian_api.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import os
+import re
 import json
 import requests
+import nltk
 
 def request_theguardian(params):
 
@@ -17,14 +19,37 @@ def request_theguardian(params):
             params['page'] = current_page
             r = requests.get('http://content.guardianapis.com/search', params=params)
             response = json.loads(r.text)
-            path = os.path.join(os.path.abspath(params['q']), params['q']+'_page_'+str(current_page)+'.json')
-            with open(path, 'w') as outfile:
-                json.dump(response, outfile)
+
+            ### --DO SOMETHIMG HERE--
+
+            ### Example 1: collect sentences from each article that contain 'Amsterdam'
+            articles = collect_sents(response, 'Amsterdam')
+            [[print (sent+'\n') for sent in article] for article in articles]
+            
+            ### Example 2: dump a response as json file 
+            dump_json(params['q'], current_page, response)
+
+            ### --DO SOMETHING HERE--
+
     else: pass
+
+def dump_json(query, current_page, response):
+    if not os.path.exists(query):
+        os.makedirs(query)
+    else: pass
+    path = os.path.join(os.path.abspath(query), query+'_page_'+str(current_page)+'.json')
+    with open(path, 'w') as f:
+        json.dump(response, f)
+ 
+def collect_sents(response, keyword):
+    raw_articles = [re.sub('<[^>]*>', '', article['fields']['body']) for article in response['response']['results']] 
+    sent_tokenized_articles = [nltk.sent_tokenize(i) for i in raw_articles]
+    keyword_sentences = [[sent for sent in article if keyword in sent]for article in sent_tokenized_articles]
+    return keyword_sentences
 
 def main():
     params = {
-        'api-key': 'aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee', ### your api-key         
+        'api-key': 'aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee', ### your api-key        
         'q': 'Amsterdam',
         'show-tags': 'keyword',
         'show-fields': 'body', 
